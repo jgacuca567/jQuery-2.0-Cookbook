8e52ba9d8d8420ada9505639f16a18a6e087035e
James Van Waza <jgacuca@gmail.com>
2015-01-08 18:25:07 -0500

    The index 28 and search php page is not working even though the database is connecting

---
+++ Chapter 3/search.php
@@ -0,0 +1,40 @@
+<?php 
+	/*Prepare an object to hold data we are going to send back to the jQuery*/
+	$data = new stdClass;
+	$data->success = false;
+	$data->results = array();
+	$data->error = NULL;
+
+	//Has the text been posted?
+	if (isset($_POST['text'])) {
+		//Connect to the database
+		require_once 'mysqli-connect.php';
+
+		//Escape the text to prevent SQL Injection
+		$text = $db->real_escape_string($_POST['text']);
+
+		/*Run a LIKE query to search for titles that are like the entered text*/
+		$q = "SELECT * FROM `stationary` WHERE title LIKE '%{$text}%'";
+		$result = $db->query($q);
+		//Did the query complete successfully?
+		if (!$result) {
+			//If not add an error to the data array
+			$data->error = "Could not query database for search results, MYSQL ERROR: " . $db->error;
+		}
+		else{
+			//Loop through the results and add to the results array
+			while ($row = $result->fetch_assoc()) {
+				$data->results[] = array(
+					'id' => $row['id'],
+					'title' => $row['title']
+					);
+			}
+			//Everything went to plan so set success to true
+			$data->success = true;
+		}
+	}
+	/*Ensure the browser is expecting the correct content type format and charset*/
+	header("Content-Type: application/json; charset=UTF-8");
+	/*JSON Encode the Data*/
+	echo json_encode($data);
+?>
