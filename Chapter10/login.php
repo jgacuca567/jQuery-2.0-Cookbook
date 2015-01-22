6aa50c3d33f9120e5f32d578e05e3ca86442b1d2
James Van Waza <jgacuca@gmail.com>
2015-01-22 09:46:07 -0500

    The login page was showing errors did not work, will have to do Creating a custom login registration again

--- Chapter10/login.php
+++ Chapter10/login.php
@@ -1,38 +1,37 @@
 <?php
-	session_start();
-	require_once ('mysqli-connect.php');
-	$username = isset($_POST['username']) ? strtolower($_POST['username']) : "";
-	$password = isset($_POST['password']) ? $_POST['password'] : "";
+session_start();
+require_once("mysqli-connect.php");
+$username = isset($_POST['username']) ? strtolower($_POST['username']) : "";
+$password = isset($_POST['password']) ? $_POST['password'] : "";
 
-	$response = array(
-		'success' => false,
-		'errors' => "",
-		'user' => array()
-	);
+$response = array(
+    "success" => false,
+    "error" => "",
+    "user" => array()
+);
 
-	$query = "SELECT `id` FROM `user` WHERE `username` = ? AND `password` = ? LIMIT 1";
-	$stmt = $mysqli->stmt_init();
-	if ($stmt->prepare($query)) {
-		$password = crypt($password, $pwsalt);
-		$stmt->bind_param("ss", $username, $password);
-		if ($stmt->execute()) {
-			$res = $stmt->get_result();
-			if($res->num_rows > 0) {
-				$row = $res->fetch_assoc();
-				$response["success"] = true;
-				$_SESSION['uid'] = $response["user"]["id"] = $row["id"];
-				$_SESSION['username'] = $response["user"]["username"] = $username;
-			} else{
-				$response["error"] = "Incorrect username or password";
-			}
-		} else{
-			$response["error"] = "Could not execute query";
-		}
-	} else{
-		$response["error"] = "Could not query database";
-	}
-	$stmt->close();
-	$mysqli->close();
-	header("Content-Type: application/json; charset=UTF-8");
-	echo json_encode($response);
-?>
+$query = "SELECT `id` FROM `user` WHERE `username` = ? AND `password` = ? LIMIT 1";
+$stmt = $mysqli->stmt_init();
+if ($stmt->prepare($query)) {
+    $password = crypt($password, $pwsalt);
+    $stmt->bind_param("ss", $username, $password);
+    if ($stmt->execute()) {
+        $res = $stmt->get_result();
+        if ($res->num_rows > 0) {
+            $row = $res->fetch_assoc();
+            $response["success"] = true;
+            $_SESSION['uid'] = $response["user"]["id"] = $row["id"];
+            $_SESSION['username'] = $response["user"]["username"] = $username;
+        } else {
+            $response["error"] = "Incorrect username or password";
+        }
+    } else {
+        $response["error"] = "Could not execute query";
+    }
+} else {
+    $response["error"] = "Could not query database";
+}
+$stmt->close();
+$mysqli->close();
+header("Content-Type: application/json; charset=UTF-8");
+echo json_encode($response);
--- Chapter10/mysqli-connect.php
+++ Chapter10/mysqli-connect.php
@@ -1,7 +1,7 @@
 <?php
-    $dbcon = new mysqli("localhost", "admin9997", "WP6cV+~SqUU~w#Nzu9", "jquery2webdbcookbook");
-    if ($dbcon->connect_errno) {
-    	die("Failed to Connect to MySQL: (". $dbcon->connect_errno . ") " . $dbcon->connect_error);
+    $mysqli = new mysqli("localhost", "admin9997", "WP6cV+~SqUU~w#Nzu9", "jquery2webdbcookbook");
+    if ($mysqli->connect_errno) {
+    	die("Failed to Connect to MySQL: (". $mysqli->connect_errno . ") " . $mysqli->connect_error);
     }
     $pwsalt = "WP6cV+~SqUU~w#Nzu9";
 ?>
