520f2b62f9bb3b7da5b19f874b26789092c9e47a
James Van Waza <jgacuca@gmail.com>
2015-03-28 13:00:10 -0400

    Using son encode to view data

---
+++ Chapter3/request-2.php
@@ -0,0 +1,30 @@
+<?php 
+	/**
+	 * Create an array of people
+	 */
+	$people = array(
+			1 => array(
+				'firstname' => 'Luke',
+				'lastname' => 'Skywalker'
+				), 
+			2 => array(
+				'firstname' => 'Darth',
+				'lastname' => 'Vader'
+				), 
+			3 => array(
+				'firstname' => 'Mace',
+				'lastname' => 'Windu'
+				)
+		);
+
+	/**
+	 * Ensure the browser is expecting the correct content type format and charset
+	 */
+	header ("Contetn-Type: application/json; charset=UTF-8");
+	 
+	/**
+	 * Encode the array of people into JSON data
+	 */
+	echo json_encode($people)
+	
+?>
