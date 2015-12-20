9cc0cf15caa227403203f7fead8b207e6f4c4427
James Van Waza <jgacuca@gmail.com>
2015-12-19 22:45:04 -0500

    Matched the mysql-connect with the document page.

--- Chapter3/mysqli-connect.php
+++ Chapter3/mysqli-connect.php
@@ -1,6 +1,4 @@
 <?php
-/** This file provides the information for accessing the database sand coonnecting to MySQL. It also sets the language coding to utf-8 */
-
 /** Define constants */
 $dbhost = 'localhost';
 $dbuser = 'admin9971';
@@ -10,13 +8,7 @@ $dbname = 'jquerycookbook';
 $db = new mysqli($dbhost, $dbuser, $dbpass);
 
 $db->select_db($dbname);
-if ($db->connect_errno > 0) {
-	die('ERROR! - COULD NOT CONNECT TO MySQL DATABASE: ' . $db->connect_error);
+if ($db->connect_errno > 0) 	die('Error! - Could not connect to MYSQL: ' . $db->connect_error);
 }
-
-/** Next we assign the database connection to a variable that we will call $dbcon */
-$dbcon = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die('Could not connect to MYSQL: ' . mysqli_connect_error());
-
-/** Finally, we set the language encoding as utf-8 */
-mysqli_set_charset($dbcon, 'utf8');
 ?>
+     
