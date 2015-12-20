3abf017accc4ac8328f5a5b2fda5d31634bb915c
James Van Waza <jgacuca@gmail.com>
2015-12-19 19:33:19 -0500

    Update the grunt file to check for all .html files

--- gruntfile.js
+++ gruntfile.js
@@ -44,7 +44,7 @@ module.exports = function(grunt) {
                 tasks: ['compass']
             },
             html: {
-                files: ['*.html']
+                files: ['{,**/}*.html']
             }, //html
             all: {
                 files: '{,**/}*.js',
