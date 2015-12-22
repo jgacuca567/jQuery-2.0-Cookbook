6b908d86b8055888d1f5f1d336d6d2a55af782ba
James Van Waza <jgacuca@gmail.com>
2015-12-22 12:15:55 -0500

    Update of the mysql-connect page

--- Chapter3/mysqli-connect.php
+++ Chapter3/mysqli-connect.php
@@ -1,14 +1,14 @@
 <?php
 /** Define constants */
-$dbhost = 'localhost';
-$dbuser = 'admin9971';
-$dbpass = '8J]9D^d#v68}&xa7+(ov;(3A644x7=';
-$dbname = 'jquerycookbook';
+$dbhost = 'localhost'; //hostname
+$dbuser = 'admin9971'; //database username
+$dbpass = '8J]9D^d#v68}&xa7+(ov;(3A644x7='; //database password
+$dbname = 'jquerycookbook'; //database name
 
 $db = new mysqli($dbhost, $dbuser, $dbpass);
 
 $db->select_db($dbname);
-if ($db->connect_errno > 0) 	die('Error! - Could not connect to MYSQL: ' . $db->connect_error);
+if ($db->connect_errno > 0) {
+	die('Error! - Could not connect to MYSQL: ' . $db->connect_error);
 }
 ?>
-     
