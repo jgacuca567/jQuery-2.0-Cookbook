20c07b2bca08294c82cc5905247decff92e47a96
JamesVanWaza <jgacuca@gmail.com>
2016-05-31 22:41:43 -0400

    PSR-4 Format

--- Chapter-3/request-2.php
+++ Chapter-3/request-2.php
@@ -1,23 +1,22 @@
 <?php
-	/** Create an array of people */
-	$people = array(
-		1 => array(
-			'firstname' => 'Luke',
-			'lastname' => 'Skywalker',
-		),
-		2 => array(
-			'firstname' => 'Darth',
-			'lastname' => 'Vader',
-		),
-		3 => array(
-			'firstname' => 'Mace',
-			'lastname' => 'Windu',
-		),
-	);
+/** Create an array of people */
+$people = array(
+    1 => array(
+        'firstname' => 'Luke',
+        'lastname' => 'Skywalker',
+    ),
+    2 => array(
+        'firstname' => 'Darth',
+        'lastname' => 'Vader',
+    ),
+    3 => array(
+        'firstname' => 'Mace',
+        'lastname' => 'Windu',
+    ),
+);
 
-	/** Ensure the browser is expecting the correct content type format and charset */
-	header("Content-Type: application/json; charset=UTF-8");
+/** Ensure the browser is expecting the correct content type format and charset */
+header("Content-Type: application/json; charset=UTF-8");
 
-	/** Encode the array of people into JSON data */
-	echo json_encode($people);
-?>
+/** Encode the array of people into JSON data */
+echo json_encode($people);
