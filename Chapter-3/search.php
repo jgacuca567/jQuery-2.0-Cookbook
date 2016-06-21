87c1183a9d4b9997a9c94e2f14768ae59ec213e5
James Van Waza <jgacuca@gmail.com>
2016-06-21 15:06:58 -0400

    PSR-4 Format

--- Chapter-3/search.php
+++ Chapter-3/search.php
@@ -8,31 +8,31 @@ $data->error = null;
 /** Has the text been posted? */
 if (isset($_POST['text'])) {
 
-    /** Connect to the database */
-    require_once 'mysqli-connect.php';
+	/** Connect to the database */
+	require_once 'mysqli-connect.php';
 
-    /** Escape the text to prevent SQL injection */
-    $text = $db->real_escape_string($_POST['text']);
+	/** Escape the text to prevent SQL injection */
+	$text = $db->real_escape_string($_POST['text']);
 
-    /** Run a LIKE query to search for titles that are like the entered text */
-    $q = "SELECT * FROM `stationary` WHERE `title` LIKE '%{$text}%'";
-    $result = $db->query($q);
+	/** Run a LIKE query to search for titles that are like the entered text */
+	$q = "SELECT * FROM `stationary` WHERE `title` LIKE '%{$text}%'";
+	$result = $db->query($q);
 
-    /** Did the query complete successfully? */
-    if (!$result) {
-        /** If not add an error to the data array */
-        $data->error = "Could not query database for search results, MYSQL ERROR: " . $db->error;
-    } else {
-        /** Loop through the results and add to the results array */
-        while ($row = $result->fetch_assoc()) {
-            $data->results[] = array(
-                'id' => $row['id'],
-                'title' => $row['title'],
-            );
-        }
-        /** Everything went to plan so set success to true */
-        $data->success = true;
-    }
+	/** Did the query complete successfully? */
+	if (!$result) {
+		/** If not add an error to the data array */
+		$data->error = "Could not query database for search results, MYSQL ERROR: " . $db->error;
+	} else {
+		/** Loop through the results and add to the results array */
+		while ($row = $result->fetch_assoc()) {
+			$data->results[] = array(
+				'id' => $row['id'],
+				'title' => $row['title'],
+			);
+		}
+		/** Everything went to plan so set success to true */
+		$data->success = true;
+	}
 }
 /** Set the content type for a json object and ensure charset is UTF-8. Not utf8 otherwise it will not work in IE*/
 header("Content-Type: application/json; charset=UTF-8");
